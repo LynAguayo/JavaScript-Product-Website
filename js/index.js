724522da6238ba8f81da389e30c9b8cab9c42912
LynAguayo <lyn.aguayoc@gmail.com>
2025-04-22 23:35:47 +0200

    Funciones para las flechas del slider

---
+++ js/index.js
@@ -0,0 +1,24 @@
+/*Slider*/
+let items = document.querySelectorAll('.slider .list .item');
+let next = document.getElementById('next');
+let prev = document.getElementById('prev');
+let thumbnails = document.querySelectorAll('.thumbnail .item');
+
+let countItem = items.length;
+let itemActive = 0;
+
+next.onclick = function(){
+    itemActive = itemActive + 1;
+    if(itemActive >= countItem){
+        itemActive = 0;
+    }
+    showSlider();
+}
+
+prev.onclick = function(){
+    itemActive = itemActive - 1;
+    if(itemActive < 0){
+        itemActive = countItem - 1;
+    }
+    showSlider();
+}
